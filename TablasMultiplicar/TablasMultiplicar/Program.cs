3225bec1a6e28c3416227b784b2088cd7920c1be
Alexander Garnica <131830717+Dantiann@users.noreply.github.com>
2024-09-03 15:21:46 -0500

    Add files via upload

---
+++ TablasMultiplicar/TablasMultiplicar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TablasMultiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un número del 1 al 10");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+                for (int j = 1; j <= numero; j++)
+                {
+                    int resultado1 = i * j;
+                    Console.WriteLine(i + "*" + j + " = " + resultado1);
+                }
+
+            Console.WriteLine("*************");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado2 = numero * i;
+                Console.WriteLine(numero + "x" + i + " = " + resultado2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
